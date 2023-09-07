bdd463de53f379a756cbbcb84d8140f6c210cc6b
twinstarhub <golddev9697@gmail.com>
2023-09-08 03:20:52 +0900

    remove the limit

--- api/index.py
+++ api/index.py
@@ -1,8 +1,12 @@
 import os
 from flask import Flask,jsonify
 from duckduckgo_search import DDGS
-app = Flask(__name__)
 
+from pymongo import MongoClient, UpdateOne
+from pymongo.server_api import ServerApi
+from pymongo.errors import BulkWriteError, ConnectionFailure
+
+app = Flask(__name__)
 
 def cron_job(fullname):
     query = f"site:linkedin.com {fullname} profile"
@@ -20,17 +24,30 @@ def cron_job(fullname):
                 layout=None,
                 license_image=None,
             )
-            count = 0
+            # count = 0
             for r in ddgs_images_gen:
                 if 'linkedin.com/in' in r['url']:
                     result.append(r)
-                    count += 1
-                    if count > 10:
-                        break
+                    # count += 1
+                    # if count > 10:
+                    #     break
 
         except Exception as ex:
             print(str(ex))
     return result
+
+
+def connect_mongo():
+    """Connect to MongoDB."""
+    uri = f"mongodb+srv://{os.getenv('MONGO_USERNAME', None)}:{os.getenv('MONGO_PASSWORD', None)}@{os.getenv('MONGO_HOST', 'localhost')}/?retryWrites=true&w=majority"
+    try:
+        client = MongoClient(uri, server_api=ServerApi('1'))
+        print('Connect to MongoDB.')
+
+    except Exception:
+        print('Unable to connect to MongoDB.')
+
+
 @app.route('/')
 def home():
     result = cron_job("Michael")
